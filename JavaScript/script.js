3f94c31e2dbfc6ac2a698e9f4695e6f09b925069
AQIB KALWAR <134145540+aqibali25@users.noreply.github.com>
2024-03-22 17:42:38 +0500

    Solve some errors in responsivness

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,21 @@
+// Select the menuButton and navAndSignup elements
 const menuButton = document.getElementById("menuButton");
 const navAndSignup = document.getElementById("navAndSignup");
 
-menuButton.addEventListener("click", () => {
-  if (navAndSignup.style.width === "0px") {
-    navAndSignup.style.width = "100%";
-  }
-  if ((navAndSignup.style.width = "100%")) {
-    navAndSignup.style.width = "0px";
+// Add click event listener to the menuButton
+menuButton.addEventListener("click", function () {
+  // Check if the window screen size is less than or equal to 768px
+  if (window.innerWidth <= 768) {
+    // Check if navAndSignup width === 0px
+    if (navAndSignup.offsetWidth === 0) {
+      // Set width for navAndSignup to 100%
+      navAndSignup.style.width = "100%";
+    } else {
+      // Set width for navAndSignup to 0
+      navAndSignup.style.width = "0";
+    }
+  } else {
+    // If window size is greater than 768px, set navAndSignup width to 60%
+    navAndSignup.style.width = "60%";
   }
 });
