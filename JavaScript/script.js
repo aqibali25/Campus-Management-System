61a558e36135362e500814cb4fc8c1389fd82c50
AQIB KALWAR <134145540+aqibali25@users.noreply.github.com>
2024-04-16 18:26:51 +0500

    working on responsiveness

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,8 +1,6 @@
 const checkBox = document.getElementById("check");
 const navAndSignup = document.getElementById("navAndSignup");
-if (window.screen.width >= "768") {
-  navAndSignup.style.width = "60%";
-}
+
 checkBox.addEventListener("change", () => {
   if (navAndSignup.style.width === "100%") {
     navAndSignup.style.width = "0%";
