545f0c93f5e4cc39637114dcc115364ef295fd6c
AQIB KALWAR <134145540+aqibali25@users.noreply.github.com>
2024-04-22 15:05:32 +0500

    working on responsiveness

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,3 +8,37 @@ checkBox.addEventListener("change", () => {
     navAndSignup.style.width = "100%";
   }
 });
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".trandingImages img");
+const totalSlides = slides.length;
+let intervalId;
+
+showSlides();
+
+function showSlides() {
+  for (let i = 0; i < totalSlides; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex].style.display = "block";
+  slides[slideIndex].style.width = "100%";
+  clearInterval(intervalId);
+  intervalId = setInterval(nextSlide, 3000); // Auto slide every 3 seconds
+}
+
+// function prevSlide() {
+//   slideIndex--;
+//   if (slideIndex < 0) {
+//     slideIndex = totalSlides - 1;
+//   }
+//   showSlides();
+// }
+
+// function nextSlide() {
+//   slideIndex++;
+//   if (slideIndex >= totalSlides) {
+//     slideIndex = 0;
+//   }
+//   showSlides();
+// }
